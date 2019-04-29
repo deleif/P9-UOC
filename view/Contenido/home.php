5a0787001469f5b47ea1db60f9d2bb95e5abd01a
agarridogar <35024653+agarridogar@users.noreply.github.com>
2019-04-29 16:03:09 +0200

    cambio imagenes

--- view/Contenido/home.php
+++ view/Contenido/home.php
@@ -9,21 +9,21 @@
                     </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                        <img class="d-block w-100" src="assets/images/slide1.jpg" alt="First slide">
+                        <img class="d-block w-100" src="assets/images/slide_1.jpg" alt="First slide">
                             <div class="carousel-caption">
                                 <h3>JDA</h3>
                                 <p>La mejor web de valoración de cervezas</p>
                             </div>
                         </div>
                         <div class="carousel-item">
-                        <img class="d-block w-100" src="assets/images/slide2.jpg"" alt="Second slide">
+                        <img class="d-block w-100" src="assets/images/slide_2.jpg"" alt="Second slide">
                             <div class="carousel-caption">
                                 <h3>Valora tus cervezas favoritas</h3>
                                 <p>Tu opinión es muy importante.</p>
                             </div>
                         </div>
                         <div class="carousel-item">
-                        <img class="d-block w-100" src="assets/images/slide3.jpg"" alt="Third slide">
+                        <img class="d-block w-100" src="assets/images/slide_3.jpg"" alt="Third slide">
                             <div class="carousel-caption">
                                 <h3>Descubre nuevas marcas</h3>
                                 <p>Sabemos que eres un amante de la cerveza y que te encantará descubrir nuevos sabores.</p>
