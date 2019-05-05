0fa987ce6fec701f8ad038804846843b6ec34a79
deleif <daniel.deleiva@gmail.com>
2019-05-05 10:08:12 +0200

    Actualización vista valoraciones

--- view/Contenido/vista_valora_producto.php
+++ view/Contenido/vista_valora_producto.php
@@ -1,29 +1,27 @@
 <LINK REL=StyleSheet HREF="assets/css/estilo_estrellas.css" TYPE="text/css" MEDIA=screen>
-<table class="table table-striped">
+<LINK REL=StyleSheet HREF="assets/css/estilo_comentarios.css" TYPE="text/css" MEDIA=screen>
 
-    <thead>
+
+<div class="row">
+  <div class=".col-lg-auto col-centrada "  >
+  <br>
+  <div class="card bg-light" style="width: 18rem;">  
+  <div class="card-body">
+
+  <h5 class="card-title"><?php echo $prodsel->nombre_producto; ?></h5>
+  <h6 class="card-subtitle mb-2 text-muted"><?php echo $prodsel->id_categoria; ?></h6>
+    
+    <table class="table  ">
+    <tbody>    
+
         <tr>
-            <th>Categoria</th>
-			<th>Producto</th>
-            <th>Descripcion</th>
-            <th style="width:120px;">Imagen</th>
-            <th style="width:120px;">Número de votos</th>
-			<th style="width:120px;">Puntuación total</th>
-			<th style="width:120px;">Puntuación media</th>
-        </tr>
-    </thead>
-    <tbody>
-
-
-        <tr>
-            <td><?php echo $prodsel->id_categoria; ?></td>
-            <td><?php echo $prodsel->nombre_producto; ?></td>
             <td><?php echo $prodsel->descripcion_producto; ?></td>		
+        </tr>
+        <tr>
             <td><img src="<?php echo $prodsel->ruta_foto;?>" alt="" /></td>
-			<td><?php echo $prodsel->num_votos; ?></td>
-			<td><?php echo $prodsel->puntos_total; ?></td>
-			<td><?php echo $prodsel->puntos_media; ?></td>
-            <td>
+        </tr>
+        <td>
+        <small>Valora este producto (1-5)</small>
                 <!--<a href="?c=Web&a=ValorarProducto&id=<?php //echo $r->id_producto; ?>">Valorar</a>-->
                 <div id="wrapper">
                     <form id="frm-valoracion" action="?c=Web&a=Votar&id_producto=<?php echo $prodsel->id_producto; ?>" method="post" enctype="multipart/form-data">
@@ -39,6 +37,7 @@
                         --><input id="radio5" type="radio" name="estrellas" value="1"><!--
                         --><label for="radio5">★</label>
                         </p>
+                        <small>Comenta este producto</small>
                         <input type="text" name="valoracion_escrita">
                         <br></br>
 
@@ -49,48 +48,53 @@
                     </form>
                 </div>
             </td>
-        </tr>
-
-    </tbody>
-</table> 
-
-
-<div class="bg-secondary">
-<br></br>
-<h3 class="text-white">Todas las valoraciones</h3>
-<br></br>
-</div>
-<div class="row">
-    
-<?php foreach($this->valoraciones->buscar_valoracion_por_producto($prodsel->id_producto) as $r): ?>
-
-  <div class=".col-lg-auto col-centrada"  >
-  <br>
-  <div class="card bg-light" style="width: 18rem;">  
-  <div class="card-body">
-    <table class="table">
-    <tbody>    
-
-    <tr>
-                <td><b><?php echo $r->nombre_producto; ?></b></td>		
             </tr>
-            <tr>
-                <td><img src="<?php echo $r->ruta_foto;?>" alt="" /></td>
-            </tr>
-            <tr>
-                <td><b>Nombre usuario:</b> <?php echo $r->nombre_usuario; ?></td>
-            </tr>
-            
-            <tr>
-                <td><b>Comentario:</b> <?php echo $r->valoracion_producto; ?></td>
-            </tr>
-            <tr>
-                <td><b>Valoración de este usuario:</b> <?php echo $r->puntos_producto_usuario; ?></td>
-            </tr>   
+  
         </tbody>
     </table>
     </div> 
     </div> 
 </div> 
-<?php endforeach; ?>
 </div>  
+<br></br>
+
+
+
+<div class="row">
+  <div class=".col-lg-auto col-centrada "  >
+<div class="bg-secondary">
+<br>
+<h4 class="text-white">Opiniones de nuestros usuarios</h4>
+<br>
+</div>
+
+<?php foreach($this->valoraciones->buscar_valoracion_por_producto($prodsel->id_producto) as $r): ?>
+<table class="table comentarios table-borderless table-responsive ">   
+    <tbody>
+    
+        <tr>
+        <td><img src="assets/images/avatar.jpeg"></td>         
+        </tr>
+        <tr>
+        <td><?php echo $r->nombre_usuario; ?></td>
+        </tr>
+        <tr>
+        <td><?php echo $r->valoracion_producto;; ?></td>
+        </tr>
+        <tr>
+        <td><small>Puntuación del usuario:  <?php echo $r->puntos_producto_usuario; ?></smal></td>	
+        </tr>
+        <tr>
+            <br>
+            <br>
+            <hr>
+        </tr>
+        
+    </tbody>
+    
+</table> 
+
+
+<?php endforeach; ?>
+</div>
+</div>
